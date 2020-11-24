ea2306e63ed4543021c359060d9e959ea5f80853
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2020-11-24 18:58:53 +0900

    CONFIGURE vendorApp

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
 
-export default function App() {
+import { AppRegistry } from "react-native";
+import { Provider } from "react-redux";
+import { setI18nConfig } from "./src/Core/localization/IMLocalization";
+import { AppNavigator } from "./src/navigations/AppNavigation";
+import { AppearanceProvider, Appearance } from "react-native-appearance";
+// import reduxStore from "./src/redux/store";
+
+const App = (props) => {
+  const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
+
+  useEffect(() => {
+    setI18nConfig();
+    Appearance.addChangeListener(({ newColorScheme }) => {
+      setColorScheme(newColorScheme);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <Provider store={reduxStore}>
+    <Provider>
+      <AppearanceProvider>
+        <AppNavigator screenProps={{ theme: colorScheme }} />
+      </AppearanceProvider>
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+App.propTypes = {};
+
+App.defaultProps = {};
+
+AppRegistry.registerComponent("App", () => App);
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # v1_expo
+
+- redux 만들기
+- setI18nConfig(); localization에서 translation - kr.json 내용 만들기
